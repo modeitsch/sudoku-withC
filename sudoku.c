1b8ab52a08abbb6a12a54906eea4f058d9d44e78
Moshe Deitsch <41856538+modeitsch@users.noreply.github.com>
2021-06-21 13:04:28 +0300

    Update sudoku.c
    changing the code to use #defined values

--- sudoku.c
+++ sudoku.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 #define SIZE 2
+#define min_number 0
+#define max_number 4
 
 // this function taks numbers to the array
 void data_array(int array[][SIZE*SIZE], int size);
@@ -125,12 +127,12 @@ for (i = 0; i < size; i++)
 // this functions checks the input of the user
 int check_input (int input)
 {
-  while (input < 0 || input >= 10)
+  while (input < min_number || input > max_number)
     {
 
       printf ("Enter a number between 0 and 4 for 2*2 or 0-9 fir 3*3: ");
       scanf ("%d", &input);
-      if (input < 0 || input >= 10)
+      if (input < min_number || input > max_number)
 	printf ("Out of range\n");
     }
   return input;
